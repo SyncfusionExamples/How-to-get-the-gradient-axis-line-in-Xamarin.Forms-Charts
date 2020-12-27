ab3062bdc8afe55655ca533e5e9fc1955207b09f
Hemalatha <hemalathavinai@gmail.com>
2020-12-27 20:48:43 +0530

    sample included

---
+++ gradient_axis_line/ChartSample/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartSample
+{
+    public class Person
+    {
+        public double Year { get; set; }
+
+        public double Product { get; set; }
+    }
+}
---
+++ gradient_axis_line/ChartSample/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartSample
+{
+    public class ViewModel
+    {
+        public List<Person> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new List<Person>()
+            {
+                new Person { Year = 2010, Product = 200 },
+                new Person { Year = 2011, Product = 400 },
+                new Person { Year = 2012, Product = 250 },
+                new Person { Year = 2013, Product = 500 },
+                new Person { Year = 2014, Product = 150 },
+                new Person { Year = 2015, Product = 375 },
+
+            };
+        }
+    }
+}
